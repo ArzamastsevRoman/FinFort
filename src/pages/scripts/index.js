9ba630c4c17bbc90dd4ca09177faa23aa620f143
Roman Arzamastsev <arzamastsevroma@yandex.ru>
2020-03-05 01:14:50 +0300

    add all blocks and adpative

--- src/pages/scripts/index.js
+++ src/pages/scripts/index.js
@@ -7,99 +7,19 @@ import '../../blocks/body/body.css'
 import '../../blocks/checkbox/checkbox.css'
 import '../../blocks/header/header.css'
 import '../../blocks/cause/cause.css'
+import '../../blocks/description/description.css'
+import '../../blocks/finance/finance.css'
+import '../../blocks/success/success.css'
+import '../../blocks/feedback/feedback.css'
 
 import '../../blocks/footer/footer.css'
-import '../../blocks/author/author.css'
-import '../../blocks/preloader/preloader.css'
-import '../../blocks/not-found/not-found.css'
-import '../../blocks/result/result.css'
-import '../../blocks/visually-hidden/visually-hidden.css'
 
-import Validate from './validate/validate'
-import Api from './api/api'
-import CardList from './cardList/cardList'
+// const imageLink = document.querySelector('.search__image');
 
-const input = document.querySelector('.search__input');
-const contentInput = document.querySelector('.search__content-input');
+// imageLink.addEventListener('mouseover', function() {
+//     imageLink.setAttribute('src', './images/clipsGreen.png');
+// })
 
-const validate = new Validate (contentInput);
-
-const button = document.querySelector('.search__button');
-
-const today = new Date();
-const to = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
-
-const date = new Date();
-const dayPrevious = 7;
-date.setDate(today.getDate() - dayPrevious);
-const from = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-const urlNews = 'https://newsapi.org/v2/everything';
-const tokenNews = '8c6a0a5107914e788d6cfa5136d5282e';
-
-
-const apiNews = new Api (urlNews, tokenNews);
-
-const preloader = document.querySelector('.preloader');
-const result = document.querySelector('.result');
-const notFound = document.querySelector('.not-found');
-const error = document.querySelector('.result__error');
-
-export {preloader, result, notFound, error}
-
-const resultContent = document.querySelector('.result__content');
-
-function createCard (arr) {
-    input.value = localStorage.getItem('request');
-    result.setAttribute('style', 'display: flex'); 
-    const startCardList = new CardList (resultContent, arr);
-    startCardList.delete();
-    startCardList.render();
-    startCardList.check();
-}
-
-
-let arrayStart = [];
-for (let i=0; i<localStorage.length; i++) {
-    if (localStorage.getItem(i)) {
-        arrayStart.push(JSON.parse(localStorage.getItem(i)));
-    }
-}
-
-button.addEventListener('click', () => {
-    preloader.setAttribute('style', 'display: block');
-    apiNews.getNews(input.value, from, to)
-        .then((data) => {
-            if (data.articles.length === 0) {
-                notFound.setAttribute('style', 'display: flex');
-                result.setAttribute('style', 'display: none');
-                preloader.setAttribute('style', 'display: none');
-                localStorage.clear();
-            } else if (data.articles.length>0) {
-                localStorage.clear();
-                localStorage.setItem('resultEver', data.totalResults);
-                localStorage.setItem('request', input.value);
-                apiNews.addNewsLocalStorage(data.articles);
-                apiNews.resultTitle(data.articles);
-                arrayStart = []; 
-                for (let i=0; i<localStorage.length; i++) {
-                    if (localStorage.getItem(i)) {
-                        arrayStart.push(JSON.parse(localStorage.getItem(i)));
-                    }
-                } 
-                console.log(arrayStart);
-                createCard(arrayStart);
-            }
-        })
-		.catch(err => { 
-            alert(`${err}: ${err.status}`);
-            error.setAttribute('style', 'display: block'); 
-            result.setAttribute('style', 'display: none');
-            preloader.setAttribute('style', 'display: none');
-			console.log(`catch err: ${err}: ${err.status}`); 
-        });;
-})
-
-if (localStorage.getItem('resultEver')){
-    createCard(arrayStart);
-}
-
+// imageLink.addEventListener('click', function() {
+//     imageLink.setAttribute('src', './images/clipsViolet.png');
+// })
